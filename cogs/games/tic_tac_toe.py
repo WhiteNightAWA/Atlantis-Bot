90888c9d87afcc20195b29f15f7a9a5307abd77b
White_Night_awa <s121101.evantang.2015@gmail.com>
2021-07-29 19:30:20 +0800

    Update tic_tac_toe.py

--- cogs/games/tic_tac_toe.py
+++ cogs/games/tic_tac_toe.py
@@ -11,61 +11,73 @@ html1 = os.getenv("html1")
 html2 = os.getenv("html2")
 
 def to_emoji(num):
-    count = 1
-    for x in ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]:
-        if count == num:
-            return x
-        count += 1
+	count = 1
+	for x in ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]:
+		if count == num:
+			return x
+		count += 1
 
-async def get_text(list_str):
-    msg, count, num = "", 1, 1
-    for row in list_str:
-        c = 1
-        for item in row:
-            if c < 3:
-                if item == 0:
-                    msg = msg + f"{to_emoji(num)} | "
-                elif item == 1:
-                    msg = msg + ":x: | "
-                elif item == 2:
-                    msg = msg + ":o: | "
-            else:
-                if count < 3:
-                    if item == 0:
-                        msg = msg + f"{to_emoji(num)}\n----+----+----\n"
-                    elif item == 1:
-                        msg = msg + ":x:\n----+----+----\n"
-                    elif item == 2:
-                        msg = msg + ":o:\n----+----+----\n"
-                else:
-                    if item == 0:
-                        msg = msg + f"{to_emoji(num)}"
-                    elif item == 1:
-                        msg = msg + ":x:"
-                    elif item == 2:
-                        msg = msg + ":o:"
-            c += 1
-            num += 1
-        count += 1
-    return msg
+async def get_text(list_str, kind):
+	msg, count, num = "", 1, 1
+	for row in list_str:
+		c = 1
+		for item in row:
+			if c < 3:
+				if item == 0:
+					if kind == 1:
+						msg = msg + f"{to_emoji(num)} | "
+					else:
+						msg = msg + f":black_large_square: | "
+				elif item == 1:
+					msg = msg + ":x: | "
+				elif item == 2:
+					msg = msg + ":o: | "
+			else:
+				if count < 3:
+					if item == 0:
+						if kind == 1:
+							msg = msg + f"{to_emoji(num)}\n----+----+----\n"
+						else:
+							msg = msg + f":black_large_square:\n----+----+----\n"
+					elif item == 1:
+						msg = msg + ":x:\n----+----+----\n"
+					elif item == 2:
+						msg = msg + ":o:\n----+----+----\n"
+				else:
+					if item == 0:
+						if kind == 1:
+							msg = msg + f"{to_emoji(num)}"
+						else:
+							msg = msg + f":black_large_square:"
+					elif item == 1:
+						msg = msg + ":x:"
+					elif item == 2:
+						msg = msg + ":o:"
+			c += 1
+			num += 1
+		count += 1
+	return msg
 
-async def check_winer(line:list):
-    winer = "no"
-    for x in line:
-        if x[0] == x[1] == x[2]:
-            if x[0] != 0:
-                winer = x[0]
-    for x in range(3):
-        if line[0][x] == line[1][x] == line[2][x]:
-            if line[x][0] != 0:
-                winer = line[x][0]
-    if line[0][0] == line[1][1] == line[2][2]:
-        if line[0][0] != 0:
-            winer = line[0][0]
-    if line[0][2] == line[1][1] == line[2][0]:
-        if line[0][2] != 0:
-            winer = line[0][0]
-    return winer
+async def check_winer(line:list, can_do):
+	winer = "no"
+	if can_do != []:
+		for x in line:
+			if x[0] == x[1] == x[2]:
+				if x[0] != 0:
+					winer = x[0]
+		for x in range(3):
+			if line[0][x] == line[1][x] == line[2][x]:
+				if line[x][0] != 0:
+					winer = line[x][0]
+		if line[0][0] == line[1][1] == line[2][2]:
+			if line[0][0] != 0:
+				winer = line[0][0]
+		if line[0][2] == line[1][1] == line[2][0]:
+			if line[0][2] != 0:
+				winer = line[0][0]
+	else:
+		winer = "draw"
+	return winer
 
 class tic_tac_toe(core):
 
@@ -90,7 +102,7 @@ class tic_tac_toe(core):
 					data["tic_tac_toe"][str(msg.id)] = {"player": {str(ctx.author.id): 1,str(p2.id): 2}, "round": ctx.author.id, "cb": cb, "can_do":["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]}
 					for emoji in ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]:
 						await msg.add_reaction(emoji)
-					await msg.edit(embed=discord.Embed(title=f"`{ctx.author}`的回合", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{ctx.author.id}>\nP2 (:o:): <@!{p2.id}>").add_field(name="棋盤", value=await get_text(cb)))
+					await msg.edit(embed=discord.Embed(title=f"`{ctx.author}`的回合", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{ctx.author.id}>\nP2 (:o:): <@!{p2.id}>").add_field(name="棋盤", value=await get_text(cb,1)))
 					requests.put(html1, params={"id": html2}, json=data)
 				elif str(payload.emoji) == "❌":
 					await msg.edit(content="", embed=discord.Embed(title=f"`{p2}`拒絕了你的邀請", color=discord.Colour.red()))
@@ -119,7 +131,8 @@ class tic_tac_toe(core):
 							x += 1
 						y += 1
 					cb = data["tic_tac_toe"][str(payload.message_id)]["cb"]
-					winer = await check_winer(cb)
+					can_do = data["tic_tac_toe"][str(payload.message_id)]["can_do"]
+					winer = await check_winer(cb, can_do)
 					if winer == "no":
 						await msg.remove_reaction(str(payload.emoji), payload.member)
 						await msg.remove_reaction(str(payload.emoji), await self.client.get_guild(payload.guild_id).fetch_member(841678712767512597))
@@ -129,10 +142,15 @@ class tic_tac_toe(core):
 							next = await self.client.get_guild(payload.guild_id).fetch_member(int(player[0]))
 						data["tic_tac_toe"][str(payload.message_id)]["round"] = next.id
 						data["tic_tac_toe"][str(payload.message_id)]["can_do"].remove(str(payload.emoji))
-						await msg.edit(embed=discord.Embed(title=f"`{next}`的回合", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{player[0]}>\nP2 (:o:): <@!{player[1]}>").add_field(name="棋盤", value=await get_text(cb)))
+						await msg.edit(embed=discord.Embed(title=f"`{next}`的回合", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{player[0]}>\nP2 (:o:): <@!{player[1]}>").add_field(name="棋盤", value=await get_text(cb,1)))
+						requests.put(html1, params={"id": html2}, json=data)
+					elif winer == "draw":
+						await msg.edit(embed=discord.Embed(title=f"遊戲結束：平局！", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{player[0]}>\nP2 (:o:): <@!{player[1]}>").add_field(name="棋盤", value=await get_text(cb,2)))
+						await msg.clear_reactions()
+						data["tic_tac_toe"].pop(str(payload.message_id), None)
 						requests.put(html1, params={"id": html2}, json=data)
 					else:
-						await msg.edit(embed=discord.Embed(title=f"遊戲結束：`{payload.member}`勝利！", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{player[0]}>\nP2 (:o:): <@!{player[1]}>").add_field(name="棋盤", value=await get_text(cb)))
+						await msg.edit(embed=discord.Embed(title=f"遊戲結束：`{payload.member}`勝利！", color=random.randint(0, 0xffffff), description=f"P1 (:x:): <@!{player[0]}>\nP2 (:o:): <@!{player[1]}>").add_field(name="棋盤", value=await get_text(cb,2)))
 						await msg.clear_reactions()
 						data["tic_tac_toe"].pop(str(payload.message_id), None)
 						if str(payload.member.id) in data["tic_tac_toe"]["points"]:

107ac296f6dd6f33df0aa0d16b4968f284b4d4fa
White_Night_awa <s121101.evantang.2015@gmail.com>
2021-07-29 20:52:12 +0800

    Update tic_tac_toe.py

--- cogs/games/tic_tac_toe.py
+++ cogs/games/tic_tac_toe.py
@@ -60,23 +60,23 @@ async def get_text(list_str, kind):
 
 async def check_winer(line:list, can_do):
 	winer = "no"
-	if can_do != []:
-		for x in line:
-			if x[0] == x[1] == x[2]:
-				if x[0] != 0:
-					winer = x[0]
-		for x in range(3):
-			if line[0][x] == line[1][x] == line[2][x]:
-				if line[x][0] != 0:
-					winer = line[x][0]
-		if line[0][0] == line[1][1] == line[2][2]:
-			if line[0][0] != 0:
-				winer = line[0][0]
-		if line[0][2] == line[1][1] == line[2][0]:
-			if line[0][2] != 0:
-				winer = line[0][0]
-	else:
-		winer = "draw"
+	for x in line:
+		if x[0] == x[1] == x[2]:
+			if x[0] != 0:
+				winer = x[0]
+	for x in range(3):
+		if line[0][x] == line[1][x] == line[2][x]:
+			if line[x][0] != 0:
+				winer = line[x][0]
+	if line[0][0] == line[1][1] == line[2][2]:
+		if line[0][0] != 0:
+			winer = line[0][0]
+	if line[0][2] == line[1][1] == line[2][0]:
+		if line[0][2] != 0:
+			winer = line[0][0]
+	if winer == "no":
+		if can_do == []:
+			winer = "draw"
 	return winer
 
 class tic_tac_toe(core):

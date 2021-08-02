6fa7feee05471806ee31cc18fc032b1259b55f0b
Jerrekty <86293358+Jerrekty@users.noreply.github.com>
2021-08-02 09:48:55 +0800

    Update tic_tac_toe.py

--- cogs/games/tic_tac_toe.py
+++ cogs/games/tic_tac_toe.py
@@ -60,12 +60,20 @@ async def get_text(list_str, kind):
 
 async def check_winer(line:list, can_do):
 	winer = "no"
-	for x in line:
+	'''for x in line:
 		if x[0] == x[1] == x[2]:
 			if x[0] != 0:
 				winer = x[0]
 	for x in range(3):
 		if line[0][x] == line[1][x] == line[2][x]:
+			if line[x][0] != 0:
+				winer = line[x][0]'''
+	for x in range(3):
+		if line[0][x] == line[1][x] == line[2][x]:
+			if line[0][x] != 0:
+				winer = line[0][x]
+	for x in range(3):
+		if line[x][0] == line[x][1] == line[x][2]:
 			if line[x][0] != 0:
 				winer = line[x][0]
 	if line[0][0] == line[1][1] == line[2][2]:

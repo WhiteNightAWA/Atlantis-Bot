ac299b318710c861e0bab0eb76a49fc4c9f86d3a
White_Night_awa <s121101.evantang.2015@gmail.com>
2021-08-05 13:34:58 +0800

    update member_join.py

--- cogs/events/member_join.py
+++ cogs/events/member_join.py
@@ -4,13 +4,13 @@ import discord
 import random
 
 
-class member_add(core):
+class member_join(core):
 
     @commands.Cog.listener()
-    async def on_member_add(self, member):
+    async def on_member_join(self, member):
         channel = self.client.get_channel(858534917784076299)
         await channel.send(embed=discord.Embed(description=f"歡迎<@!{member.id}>來到了殘破不堪的小卯村:awa3: :awa3: :awa3: :awa3: :MuMao_1:", color=random.randint(0, 0xffffff)))
 
 
 def setup(client):
-    client.add_cog(member_add(client))
+    client.add_cog(member_join(client))

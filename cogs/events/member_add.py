3446d1332462f46f2af61ac4a226415966aa0a6b
White_Night_awa <s121101.evantang.2015@gmail.com>
2021-08-05 13:26:41 +0800

    update

--- cogs/events/member_add.py
+++ cogs/events/member_add.py
@@ -8,7 +8,7 @@ class member_add(core):
 
     @commands.Cog.listener()
     async def on_member_add(self, member):
-        channel = self.client.get_channel(872704498244661269)
+        channel = self.client.get_channel(858534917784076299)
         await channel.send(embed=discord.Embed(description=f"歡迎<@!{member.id}>來到了殘破不堪的小卯村:awa3: :awa3: :awa3: :awa3: :MuMao_1:", color=random.randint(0, 0xffffff)))
 
 

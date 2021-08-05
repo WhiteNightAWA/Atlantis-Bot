3d06b2299207c6efe77627710ebaa20bf6a27d7c
White_Night_awa <s121101.evantang.2015@gmail.com>
2021-08-05 13:57:22 +0800

    update emoji

--- cogs/events/member_join.py
+++ cogs/events/member_join.py
@@ -9,7 +9,7 @@ class member_join(core):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = self.client.get_channel(858534917784076299)
-        await channel.send(embed=discord.Embed(description=f"歡迎<@!{member.id}>來到了殘破不堪的小卯村:awa3: :awa3: :awa3: :awa3: :MuMao_1:", color=random.randint(0, 0xffffff)))
+        await channel.send(embed=discord.Embed(description=f"歡迎<@!{member.id}>來到了殘破不堪的小卯村<:awa3:866963596785221662> <:awa3:866963596785221662> <:awa3:866963596785221662> <:awa3:866963596785221662> <:MuMao_1:865847027715014677>", color=random.randint(0, 0xffffff)))
 
 
 def setup(client):
